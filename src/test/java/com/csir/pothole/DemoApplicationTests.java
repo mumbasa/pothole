b47cea4f8d3203789cbe132068e16fc767519dc9
Laryea, Bryan, Vodafone (External) <bryan.laryea@vodafone.com>
2023-01-13 23:09:28 +0000

    second commit

--- src/test/java/com/csir/pothole/DemoApplicationTests.java
+++ src/test/java/com/csir/pothole/DemoApplicationTests.java
@@ -5,11 +5,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.csir.pothole.models.Subscriber;
@@ -17,16 +18,14 @@ import com.csir.pothole.repository.SubscriberRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-
+@ContextConfiguration 
 @AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-	@Autowired
-	MockBean beaner;
 
 	@Autowired
 	private ObjectMapper objectMapper;
@@ -47,9 +46,5 @@ class DemoApplicationTests {
 		Subscriber sub = subscriberRepo.findByTelephoneNumber("0545730281").get();
 		assertThat(sub.getName()).isEqualTo(subscriber.getName());
 	}
-
-	
-	
-
 
 }
